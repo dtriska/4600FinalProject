b4d8c5c36025c74132a4a99f23021cedbbc52570
dtriska <dtriska21@yahoo.com>
2024-05-10 12:41:41 -0700

    Quick Comment

--- receiver.py
+++ receiver.py
@@ -1,5 +1,5 @@
-from cryptography.hazmat.primitives import padding, hashes
 import os
+from cryptography.hazmat.primitives import padding, hashes
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization, hmac
